03f384febc6d013315b91d2ee16d430f079d41f0
CNR <narayana.cheerla@gmail.com>
2024-09-29 12:39:00 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
 
-
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Body/>}>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
-
     </>
   );
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
     <div>
       <NavBar />
       <Outlet />
+      <Footer/>
     </div>
   );
 };
