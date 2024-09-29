720fed327100088f57ff77089d5fd0c884b956ea
CNR <narayana.cheerla@gmail.com>
2024-09-29 12:03:18 +0530

    added daisyUI tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 function App() {
-
   return (
     <>
-    <h1>Hello Dev Tinder</h1>
+      <div className="navbar bg-base-300">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">🧑‍💻Dev Tinder</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control"></div>
+          <div className="dropdown dropdown-end mx-5">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
