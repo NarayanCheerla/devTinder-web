f79daeae3c3803e883845081fc71b7f231546c53
CNR <narayana.cheerla@gmail.com>
2024-09-30 10:06:43 +0530

    restricting user unauthorize access

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (!userData)
+      try {
+        const res = await axios.get(`${BASE_URL}/profile/view`, {
+          withCredentials: true,
+        });
+        dispatch(addUser(res.data));
+      } catch (error) {
+        if (error.status === 401) navigate("/login");
+        console.log("Error", +error.message);
+      }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Outlet />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector(store => store.user);
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻Dev Tinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">🧑‍💻Dev Tinder</Link>
       </div>
       {
         user && (
@@ -31,10 +32,10 @@ const NavBar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
               <a>Settings</a>
